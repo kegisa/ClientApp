6ab692ade5f03864a0e4bce7e81d3bd719a536d0
kegisa <vit.levin2011@yandex.ru>
2018-03-12 16:28:41 +0400

    Добавлена клиентская часть

--- ClientApp/Form1.cs
+++ ClientApp/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,15 @@ namespace ClientApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ip = host.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ip, 18000);
+            Socket socket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(endPoint);
         }
     }
 }
